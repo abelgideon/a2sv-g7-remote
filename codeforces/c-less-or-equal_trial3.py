b80a75392d2cbd05e7c6cded3b1b19ffa0dfabe3
Abel Gideon <abelgideontk7@gmail.com>
2026-02-26 14:06:35 +0300

    Add solution for C - Less or Equal (trial 3)

---
+++ codeforces/c-less-or-equal_trial3.py
@@ -0,0 +1,19 @@
+n, k = map(int, input().split())
+
+nums = list(map(int, input().split()))
+nums.sort()
+
+if k == 0:
+    if nums[0] - 1 >= 1:
+        print(nums[0]-1)
+    else:
+        print(-1)
+elif k == 1:
+    if n > k and (nums[k-1] == nums[k]):
+        print(-1)
+    else:
+        print(nums[0])
+elif k < n and (nums[k-1] == nums[k]):
+    print(-1)
+else:
+    print(nums[k-1])
