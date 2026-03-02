bfb568ba059fc2c6cec3967e59147076fb6e3c83
Abel Gideon <abelgideontk7@gmail.com>
2026-03-02 08:02:50 +0300

    Add solution for Reduction Operations to Make the Array Elements Equal (trial 4)

---
+++ leetcode/reduction-operations-to-make-the-array-elements-equal_trial4.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reductionOperations(self, nums: List[int]) -> int:
+        nums.sort()
+        op = 0
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]:
+                op += len(nums) - i
+        
+        return op
