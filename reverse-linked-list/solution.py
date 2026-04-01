28d710d0cc64d45edd6d6a7c1dad045d28f24506
Abel Gideon <abelgideontk7@gmail.com>
2026-04-01 17:28:04 +0300

    Add leetcode solution (15m)

---
+++ reverse-linked-list/solution.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or head.next == None:
+            return head
+
+        prev = None
+        curr = head
+        nxt = head.next
+
+        while curr:
+            curr.next = prev
+            prev = curr
+            curr = nxt
+            if nxt:
+                nxt = nxt.next
+        
+        head = prev
+        
+        return head
+
